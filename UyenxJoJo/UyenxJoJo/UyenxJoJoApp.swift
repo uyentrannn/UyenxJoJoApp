83b6aed95467ed5297c559b37c6a9bba4b8eafc3
Uyen Tran <utra4729@uni.sydney.edu.au>
2020-08-26 22:20:27 +1000

    created figma ui design for app

---
+++ UyenxJoJo/UyenxJoJo/UyenxJoJoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UyenxJoJoApp.swift
+//  UyenxJoJo
+//
+//  Created by Uyen Tran on 26/8/20.
+//
+
+import SwiftUI
+
+@main
+struct UyenxJoJoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
