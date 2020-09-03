ca3c06ab990c4ad3da56556cb1bb77993cbb2ac7
Uyen Tran <utra4729@uni.sydney.edu.au>
2020-09-04 01:16:30 +1000

    Created first page and added image

---
+++ App/uyenxjojoapp/lib/global.dart
@@ -0,0 +1,15 @@
+// File that contains all the font styles
+import 'package:flutter/material.dart';
+
+// Colour Codes for App Theme
+Color darkpink = new Color(0xFFE09891);
+Color pink = new Color(0xFFE8B9AB);
+Color purple = new Color(0xFFE1D2ED);
+Color yellow = new Color(0xFFFDEFD8);
+Color blue = new Color(0xFFD9EDED);
+
+TextStyle buttonStyle = new TextStyle(
+  fontFamily: 'Montaga',
+  color: Colors.black,
+  fontSize: 25,
+);
