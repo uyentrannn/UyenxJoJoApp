d1a69a7aa7fdcec303c86e2c4d535c861c42374f
Uyen Tran <utra4729@uni.sydney.edu.au>
2020-09-12 01:46:06 +1000

    eddited second page and added third route

--- App/uyenxjojoapp/lib/main.dart
+++ App/uyenxjojoapp/lib/main.dart
@@ -47,10 +47,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 Container(
                   alignment: Alignment.center,
                   child:
-                      Image.asset('assets/images/bench-image.JPG', width: 320),
+                      Image.asset('assets/images/bench-image.JPG'),
                 ),
                 Container(
                   alignment: Alignment.center,
+                  padding: EdgeInsets.only(bottom: 80),
                   child: FlatButton(
                     color: yellow,
                     textColor: Colors.black,
@@ -77,40 +78,116 @@ class SecondRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: purple,
-            body: Center(
-                child: Container(
-              width: 320,
-              height: 422,
-              decoration: BoxDecoration(
-                  border: Border.all(
-                color: Colors.white,
-                width: 7,
-              )),
-              child: Stack(children: <Widget>[
-                Container(
-                  alignment: Alignment.center,
-                  child:
-                      Image.asset('assets/images/bench-image.JPG', width: 320),
+      home: Scaffold(
+        backgroundColor: purple,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Text(
+                  "Happy 2nd Anniversary",
+                  style: buttonStyle,
                 ),
-                Container(
-                  alignment: Alignment.center,
-                  child: FlatButton(
-                    color: yellow,
-                    textColor: Colors.black,
-                    padding: EdgeInsets.all(10.0),
-                    splashColor: blue,
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      "Uyen x Joseph",
-                      style: buttonStyle,
-                    ),
+              ),
+              Container(
+                width: 320,
+                height: 422,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 7,
+                  )
+                ),
+                child: Container(
+                  child:
+                      Image.asset('assets/images/lavender.png'),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 50),
+                child: FlatButton(
+                  color: yellow,
+                  textColor: Colors.black,
+                  padding: EdgeInsets.all(10.0),
+                  splashColor: blue,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ThirdRoute()),
+                    );
+                  },
+                  child: Text(
+                    "Our Journey",
+                    style: buttonStyle,
                   ),
-                )
-              ]),
-            ))));
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+
+class ThirdRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: purple,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Text(
+                  "Happy 2nd Anniversary",
+                  style: buttonStyle,
+                ),
+              ),
+              Container(
+                width: 320,
+                height: 422,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 7,
+                  )
+                ),
+                child: Container(
+                  child:
+                      Image.asset('assets/images/lavender.png'),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 50),
+                child: FlatButton(
+                  color: yellow,
+                  textColor: Colors.black,
+                  padding: EdgeInsets.all(10.0),
+                  splashColor: blue,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MyApp()),
+                    );
+                  },
+                  child: Text(
+                    "Our Journey",
+                    style: buttonStyle,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
